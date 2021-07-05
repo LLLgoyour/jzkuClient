45452cd45a67fd0a1406537cf993b03425b2b453
Shinonome Masaki <lllgoyour@outlook.com>
2021-07-05 03:03:15 -0400

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(new MyApp());
+void main() => runApp(new jzkuApp());
 
-class MyApp extends StatelessWidget {
+class jzkuApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Jzku',
+      title: '机智库',
       home: new Scaffold(
         appBar: new AppBar(
-          title: new Text('Jzku'),
+          title: new Text('机智库'),
         ),
         body: new Center(
           child: new Text('小白挨透'),
